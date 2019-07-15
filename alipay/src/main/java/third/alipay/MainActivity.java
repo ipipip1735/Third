7bbf5600283f558ba565ed6aa5e8bbc2fbd4baa0
ipipip1735 <toyota0626>
2019-07-15 08:05:22 +0800

    Initial commit

---
+++ alipay/src/main/java/third/alipay/MainActivity.java
@@ -0,0 +1,165 @@
+package third.alipay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+
+import com.alipay.sdk.app.PayTask;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2019/7/13.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("*********  " + getClass().getSimpleName() + ".onCreate  *********");
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStart  *********");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestoreInstanceState  *********");
+
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onRestart  *********");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("*********  " + getClass().getSimpleName() + ".onResume  *********");
+    }
+
+    @Override
+    protected void onPause() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onPause  *********");
+
+        super.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onBackPressed  *********");
+
+        super.onBackPressed();
+    }
+
+
+    @Override
+    protected void onStop() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onStop  *********");
+
+        super.onStop();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onSaveInstanceState  *********");
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        System.out.println("*********  " + getClass().getSimpleName() + ".onDestroy  *********");
+
+        super.onDestroy();
+    }
+
+
+    public void start(View view) {
+        System.out.println("~~button.start~~");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.READ_PHONE_STATE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    }, 1);
+
+        }
+
+
+
+
+//        final String orderInfo = info;   // 订单信息
+//
+//        Runnable payRunnable = new Runnable() {
+//
+//            @Override
+//            public void run() {
+//                PayTask alipay = new PayTask(DemoActivity.this);
+//                Map<String,String> result = alipay.payV2(orderInfo,true);
+//
+//                Message msg = new Message();
+//                msg.what = SDK_PAY_FLAG;
+//                msg.obj = result;
+//                mHandler.sendMessage(msg);
+//            }
+//        };
+//        // 必须异步调用
+//        Thread payThread = new Thread(payRunnable);
+//        payThread.start();
+
+
+    }
+
+
+    public void stop(View view) {
+        System.out.println("~~button.stop~~");
+
+    }
+
+    public void bind(View view) {
+        System.out.println("~~button.bind~~");
+
+    }
+
+    public void unbind(View view) {
+        System.out.println("~~button.unbind~~");
+
+    }
+
+    public void reloading(View view) {
+        System.out.println("~~button.reloading~~");
+
+    }
+
+
+    public void del(View view) {
+        System.out.println("~~button.del~~");
+
+    }
+
+    public void query(View view) {
+        System.out.println("~~button.query~~");
+
+    }
+}
